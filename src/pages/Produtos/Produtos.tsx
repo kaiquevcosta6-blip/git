a10dfda4a8efcb91b01aa3797cb2d018c084a55b
KAIQUE VINICIUS CARDOSO RODRIGUES DA COSTA <51110276885@info.local>
2025-11-06 17:29:40 -0300

    Componentizando os cards de produto

--- src/pages/Produtos/Produtos.tsx
+++ src/pages/Produtos/Produtos.tsx
@@ -3,13 +3,34 @@ import banner_1 from '../../assets/imgs/banner.png';
 import banner_2 from '../../assets/imgs/banner2.png';
 import banner_3 from '../../assets/imgs/banner3.png';
 import choc_belga from '../../assets/imgs/choc-belga.png';
-import choc_ninho from '../../assets/imgs/choc-ninho.png';
-import choc_cenoura_com_choc from '../../assets/imgs/cenoura-choc.png';
-import choc_morango from '../../assets/imgs/choc-ninho-morango.png';
-import choc_pistache from '../../assets/imgs/choc-pistache.png';
-import choc_oreo from '../../assets/imgs/choc-oreo.png';
+// import choc_ninho from '../../assets/imgs/choc-ninho.png';
+// import choc_cenoura_com_choc from '../../assets/imgs/cenoura-choc.png';
+// import choc_morango from '../../assets/imgs/choc-ninho-morango.png';
+// import choc_pistache from '../../assets/imgs/choc-pistache.png';
+// import choc_oreo from '../../assets/imgs/choc-oreo.png';
+import { useEffect, useState } from 'react';
+import { getBolos } from '../../services/bolosService';
+import type { Bolo } from '../../types/Bolo';
 
 export default function Produtos() {
+
+    const [bolos, setBolos] = useState<Bolo[]>([]);
+
+    const fetchBolos = async () => {
+        try {
+            const dados = await getBolos();
+            console.log("dados retornados da API", dados);
+            setBolos(dados);
+        } catch (error) {
+            console.error("erro ao executar getBolos:", error)
+        }
+    }
+
+
+    useEffect(() => {
+        fetchBolos();
+    }, [])
+
     return (
         <main>
 
@@ -19,7 +40,7 @@ export default function Produtos() {
                         <img src={banner_1} className="d-block w-100" alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src= {banner_2} className="d-block w-100" alt="..." />
+                        <img src={banner_2} className="d-block w-100" alt="..." />
                     </div>
                     <div className="carousel-item">
                         <img src={banner_3} className="d-block w-100" alt="..." />
@@ -46,6 +67,17 @@ export default function Produtos() {
                 </div>
 
                 <section className="cards">
+
+                    {
+                        bolos.map((b: Bolo) => (
+                            <div className="card_produto">
+                                <img src={`http://localhost:3000/${b.imagens[0]}`} alt="Uma fatia de bolo de chocolate belga" />
+                                <h2>{b.nome}</h2>
+                                <p></p>
+                                <span>{b.peso}</span>
+                            </div>
+                        ))
+                    }
                     <div className="card_produto">
                         <img src={choc_belga} alt="Uma fatia de bolo de chocolate belga" />
                         <h2>Chocolate Belga</h2>
@@ -53,42 +85,7 @@ export default function Produtos() {
                         <span>R$ 80,00/kg.</span>
                     </div>
 
-                    <div className="card_produto">
-                        <img src= {choc_ninho} alt="Uma fatia de bolo de chocolate com ninho" />
-                        <h2>Chocolate com Ninho</h2>
-                        <p>Bolo macio de chocolate com creme de leite Ninho, um sabor irresistível.</p>
-                        <span>R$ 90,00/kg.</span>
-                    </div>
 
-                    <div className="card_produto">
-                        <img src={choc_cenoura_com_choc}  alt="Uma fatia de bolo de cenoura com cobertura de chocolate" />
-                        <h2>Cenoura com Chocolate</h2>
-                        <p>Bolo macio de cenoura com cobertura de chocolate, um sabor família e irresistível.</p>
-                        <span>R$ 70,00/kg.</span>
-                    </div>
-
-                    <div className="card_produto">
-                        <img src= {choc_morango}
-                            alt="Uma fatia de bolo de ninho com morango e cobertura de chocolate" />
-                        <h2>Ninho com Morango</h2>
-                        <p>Bolo macio de chocolate com recheio de leite ninho e morango. A melhor combinação para seu dia.</p>
-                        <span>R$ 105,00/kg.</span>
-                    </div>
-
-                    <div className="card_produto">
-                        <img src={choc_pistache} alt="Uma fatia de bolo de chocolate com pistache" />
-                        <h2>Chocolate com Pistache</h2>
-                        <p>Bolo macio de chocolate, aplicado granulado que traz crocância e um sabor irresistível.</p>
-                        <span>R$ 80,00/kg.</span>
-                    </div>
-
-                    <div className="card_produto">
-                        <img src= {choc_oreo} alt="Uma fatia de bolo de chocolate com biscoito recheado Oreo" />
-                        <h2>Chocolate com Oreo</h2>
-                        <p>Bolo macio chocolate, com creme delicado, recheio e cobertura de biscoitos rechedos Oreo, um sabor
-                            irresistível.</p>
-                        <span>R$ 100,00/kg.</span>
-                    </div>
                 </section>
             </section>
 
